250e49f2f2ca1b3193960932dd85c8d4782a35c1
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 17:50:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,17 @@
-import os
-
 from scripts.telegram import send_message
 from scripts.bse import get_bse_announcements
-
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHANNEL_ID = os.environ["CHANNEL_ID"]
 
 try:
     data = get_bse_announcements()
 
     if data:
-        message = "✅ BSE API Connected Successfully"
+        message = "✅ BSE API Connected Successfully!"
     else:
-        message = "❌ No data received from BSE"
+        message = "❌ No data received from BSE API."
 
 except Exception as e:
     message = f"❌ Error:\n{e}"
 
-result = send_message(
-    BOT_TOKEN,
-    CHANNEL_ID,
-    message
-)
+send_message(message)
 
-print(result)
+print("Message Sent Successfully")
