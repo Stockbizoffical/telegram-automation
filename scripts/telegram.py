6e15d21f0efa0fd36f661aff0a8583e85cc46802
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 19:28:45 +0530

    Update telegram.pyi

--- scripts/telegram.py
+++ scripts/telegram.py
@@ -1,12 +1,21 @@
 import requests
 from scripts.config import BOT_TOKEN, CHANNEL_ID
 
-def send_message(text):
+
+def send_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    data = {
+    payload = {
         "chat_id": CHANNEL_ID,
-        "text": text
+        "text": message,
+        "parse_mode": "HTML",
+        "disable_web_page_preview": True
     }
 
-    requests.post(url, data=data)
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        print("✅ Telegram Message Sent Successfully")
+    else:
+        print("❌ Telegram Error")
+        print(response.text)
