404416ded8a022fd67e0b8e7867f427504f97863
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 17:46:09 +0530

    Update telegram.py

--- scripts/telegram.py
+++ scripts/telegram.py
@@ -1,3 +1,19 @@
+import os
+
+from scripts.telegram import send_message
+from scripts.bse import get_bse_announcements
+
+try:
+    data = get_bse_announcements()
+
+    if data:
+        message = "✅ BSE API Connected Successfully!"
+    else:
+        message = "❌ No data received from BSE API."
+
+except Exception as e:
+    message = f"❌ Error:\n{e}"
+
 send_message(message)
 
 print("Message Sent Successfully")
