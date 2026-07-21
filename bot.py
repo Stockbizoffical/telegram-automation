b7178cb364e1f2059ef8d4852952223973ec2ffa
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 16:29:44 +0530

    Connect bot with BSE

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 import os
+
 from scripts.telegram import send_message
+from scripts.bse import get_bse_announcements
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-message = """
-🚀 Stock Biz AI Automation
+try:
+    data = get_bse_announcements()
 
-✅ Professional Project Structure Ready
-✅ Telegram Module Connected
+    if data:
+        message = "✅ BSE API Connected Successfully"
+    else:
+        message = "❌ No data received from BSE"
 
-अब अगला Step:
-📊 BSE + NSE Automation
-🤖 AI Summary
-"""
+except Exception as e:
+    message = f"❌ Error:\n{e}"
 
-result = send_message(BOT_TOKEN, CHANNEL_ID, message)
+result = send_message(
+    BOT_TOKEN,
+    CHANNEL_ID,
+    message
+)
 
 print(result)
