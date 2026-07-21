2fc616bb3f8d54399cc8e07cfab9d798c0faf784
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 18:36:51 +0530

    Update bse.py

--- scripts/bse.py
+++ scripts/bse.py
@@ -1,18 +1,39 @@
 import requests
+from datetime import datetime
 
 URL = "https://api.bseindia.com/BseIndiaAPI/api/AnnSubCategoryGetData/w"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0",
-    "Referer": "https://www.bseindia.com/"
+    "Referer": "https://www.bseindia.com/",
 }
 
 def get_bse_announcements():
-    try:
-        response = requests.get(URL, headers=HEADERS, timeout=20)
+    today = datetime.now().strftime("%Y%m%d")
 
-        if response.status_code == 200:
-            return response.json()
+    params = {
+        "pageno": 1,
+        "strCat": "-1",
+        "strPrevDate": today,
+        "strToDate": today,
+        "strScrip": "",
+        "strSearch": "P",
+        "strType": "C",
+        "subcategory": "",
+    }
+
+    try:
+        r = requests.get(
+            URL,
+            headers=HEADERS,
+            params=params,
+            timeout=20
+        )
+
+        data = r.json()
+
+        if "Table" in data:
+            return data["Table"]
 
         return []
 
