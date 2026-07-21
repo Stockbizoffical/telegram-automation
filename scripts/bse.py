7e44feaa3a575accdc51401b9e62da5834cb5a6f
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 18:32:51 +0530

    Update bse.py

--- scripts/bse.py
+++ scripts/bse.py
@@ -1,6 +1,21 @@
+import requests
+
+URL = "https://api.bseindia.com/BseIndiaAPI/api/AnnSubCategoryGetData/w"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://www.bseindia.com/"
+}
+
 def get_bse_announcements():
-    return [
-        {
-            "title": "BSE Connection Successful"
-        }
-    ]
+    try:
+        response = requests.get(URL, headers=HEADERS, timeout=20)
+
+        if response.status_code == 200:
+            return response.json()
+
+        return []
+
+    except Exception as e:
+        print(e)
+        return []
