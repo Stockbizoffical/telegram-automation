bf40f4ad1606459a05188bd60cb8e632f0655c61
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 17:37:50 +0530

    Update telegram.py

--- scripts/telegram.py
+++ scripts/telegram.py
@@ -1,12 +1,3 @@
-import requests
-from scripts.config import BOT_TOKEN, CHANNEL_ID
+send_message(message)
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    data = {
-        "chat_id": CHANNEL_ID,
-        "text": text
-    }
-
-    requests.post(url, data=data)
+print("Message Sent Successfully")
