77d72e41893bbe45bb17a35ce6445214b6ef5c8e
Stockbizoffical <malviya.vk499@gmail.com>
2026-07-21 19:20:14 +0530

    Update storage.pyp

--- scripts/storage.py
+++ scripts/storage.py
@@ -1,13 +1,33 @@
+import json
 import os
 
-FILE_NAME = "last_news.txt"
+FILE_PATH = "data/last_news.json"
 
-def get_last_news():
-    if os.path.exists(FILE_NAME):
-        with open(FILE_NAME, "r") as f:
-            return f.read().strip()
-    return ""
 
-def save_last_news(news_id):
-    with open(FILE_NAME, "w") as f:
-        f.write(str(news_id))
+def load_data():
+    if not os.path.exists(FILE_PATH):
+        return {}
+
+    with open(FILE_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_data(data):
+    with open(FILE_PATH, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+
+def get_last_news(source):
+    data = load_data()
+    return data.get(source, {}).get("last_news_id", "")
+
+
+def save_last_news(source, news_id):
+    data = load_data()
+
+    if source not in data:
+        data[source] = {}
+
+    data[source]["last_news_id"] = news_id
+
+    save_data(data)
